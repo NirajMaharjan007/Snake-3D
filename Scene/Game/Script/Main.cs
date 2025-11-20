d3f724cebe6674a1e3a7e4c817ed87a60cbe0ae5
NirajMaharjan@ArchLinux <niraj.mhrjn770@gmail.com>
2025-11-20 22:01:06 +0545

    ADDED
    PROJECT added

---
+++ Scene/Game/Script/Main.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+
+namespace Scene.Game.Script;
+
+public partial class Main : Node3D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+
+    public override void _ExitTree() { }
+}
