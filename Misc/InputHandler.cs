9eaf1f1083aa25c8e26ae391ab7b356a0693dd60
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2025-11-28 23:05:54 +0545

    updates

--- Misc/InputHandler.cs
+++ Misc/InputHandler.cs
@@ -1,10 +1,64 @@
+using System;
 using Godot;
 
 namespace Snake3D.Misc;
 
-public partial class InputHandler : InputEventAction
+public partial class InputHandler : Node
 {
-    public Direction Order { get; set; } = Direction.DOWN;
+    [Signal]
+    public delegate void DirectionChangedEventHandler(Direction newDirection);
+
+    private static readonly Lazy<InputHandler> lazyInstance = new Lazy<InputHandler>(() => new());
+
+    public Direction CurrentDirection { get; set; } = Direction.DOWN;
 
     private InputHandler() { }
+
+    public override void _Ready() { }
+
+    public override void _Process(double delta)
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        // Handle directional input
+        var oldDirection = CurrentDirection;
+
+        if (Input.IsActionPressed("ui_up"))
+            CurrentDirection = Direction.UP;
+        else if (Input.IsActionPressed("ui_down"))
+            CurrentDirection = Direction.DOWN;
+        else if (Input.IsActionPressed("ui_left"))
+            CurrentDirection = Direction.LEFT;
+        else if (Input.IsActionPressed("ui_right"))
+            CurrentDirection = Direction.RIGHT;
+
+        // Emit signal if direction changed
+        if (oldDirection != CurrentDirection)
+        {
+            EmitSignal(SignalName.DirectionChanged);
+        }
+    }
+
+    // Public methods to check input state
+    public bool IsDirectionPressed(Direction direction)
+    {
+        return CurrentDirection == direction;
+    }
+
+    public Vector2 GetDirectionVector()
+    {
+        return CurrentDirection switch
+        {
+            Direction.UP => Vector2.Up,
+            Direction.DOWN => Vector2.Down,
+            Direction.LEFT => Vector2.Left,
+            Direction.RIGHT => Vector2.Right,
+            _ => Vector2.Zero,
+        };
+    }
+
+    public static InputHandler Instance => lazyInstance.Value;
 }
--- Scene/Game/Script/Snake.cs
+++ Scene/Game/Script/Snake.cs
@@ -8,20 +8,56 @@ public partial class Snake : Node3D
     [Export]
     public int SPEED = 1;
 
-    InputHandler handler;
+    private InputHandler handler = InputHandler.Instance;
+
+    private char direction = 'd';
 
     public override void _Ready()
     {
         base._Ready();
+
+        AddChild(handler);
+
+        switch (direction)
+        {
+            case 'u':
+                handler.CurrentDirection = Direction.UP;
+                break;
+
+            case 'd':
+                handler.CurrentDirection = Direction.DOWN;
+                break;
+
+            case 'l':
+                handler.CurrentDirection = Direction.LEFT;
+                break;
+
+            case 'r':
+                handler.CurrentDirection = Direction.RIGHT;
+                break;
+        }
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
 
-        Position += Transform.Basis.Z * 1.0f * (float)delta;
+        var position = Position;
 
-        GD.Print(Position);
+        if (handler.CurrentDirection.Equals(Direction.UP))
+        {
+            position += Transform.Basis.Z * -SPEED * (float)delta;
+        }
+        else if (handler.CurrentDirection.Equals(Direction.DOWN))
+        {
+            position += Transform.Basis.Z * SPEED * (float)delta;
+        }
+
+        Position = position;
+
+        // Position += Transform.Basis.Z * 1.0f * (float)delta;
+
+        GD.Print($"{Position} and {handler.CurrentDirection}");
     }
 
     public override void _PhysicsProcess(double delta)
