b1fd630126f91b680cc12c129f4f491d19b651df
Niraj_Arch007 <niraj.mhrjn770@gmail.com>
2026-03-19 21:33:49 +0545

    Updated Global Position
    ToDO:Fixing

--- Scene/Game/Script/Main.cs
+++ Scene/Game/Script/Main.cs
@@ -4,13 +4,26 @@ namespace Snake3D.Scene.Game.Script;
 
 public partial class Main : Node3D
 {
+    Camera3D camera;
+
+    CharacterBody3D snake;
+
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        base._Ready();
+
+        camera = GetNode<Camera3D>("Camera3D");
+        snake = GetNode<CharacterBody3D>("Snake");
+    }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
         base._Process(delta);
+
+        GD.Print($"Snake {snake.GlobalPosition}");
+        OutOfBound();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -19,4 +32,23 @@ public partial class Main : Node3D
     }
 
     public override void _ExitTree() { }
+
+    private void OutOfBound()
+    {
+        float x = snake.GlobalPosition.X;
+        float y = snake.GlobalPosition.Y;
+        float z = snake.GlobalPosition.Z;
+
+        if (x <= -18)
+            x = 18;
+        else if (x >= 18)
+            x = -18;
+
+        if (z >= 14)
+            z = -14;
+        else if (z <= -14)
+            z = 14;
+
+        snake.GlobalPosition = new(x, y, z);
+    }
 }
